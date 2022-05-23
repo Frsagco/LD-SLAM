23cdeb2c9b60f5fce9536fb18a05cc5d7b33a988
Frsagco <francesco.aglieco@live.com>
2022-05-23 11:38:32 +0200

    LD-SLAM Odometry
    Scanmatcher_component.cpp reworking...

---
+++ src/scanmatcher_node.cpp
@@ -0,0 +1,12 @@
+#include "ld_slam/scanmatcher_component.h"
+#include <rclcpp/rclcpp.hpp>
+
+int main(int argc, char * argv[])
+{
+  rclcpp::init(argc, argv);
+  rclcpp::NodeOptions options;
+  options.use_intra_process_comms(true);
+  rclcpp::spin(std::make_shared<ldslam::ScanMatcherComponent>(options));
+  rclcpp::shutdown();
+  return 0;
+}
